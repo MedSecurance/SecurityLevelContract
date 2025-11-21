2ffb514f9a9cfafdad43386d3e8ddb56173ca99a
Daniel Naro <dnaro@ac.upc.edu>
2025-11-21 14:02:34 +0100

    Adding upload to S3 server

---
+++ docker_signer/src/main/java/edu/upc/dmag/signinginterface/config/MinioConfig.java
@@ -0,0 +1,34 @@
+package edu.upc.dmag.signinginterface.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+public class MinioConfig {
+
+    @Bean
+    public S3AsyncClient s3AsyncClient() {
+        return S3AsyncClient.builder()
+                .endpointOverride(URI.create("http://signer.minio:9000"))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create("accesskey", "secretkey")
+                        )
+                )
+                .region(Region.US_EAST_1) // MinIO ignores region but required by SDK
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true) // REQUIRED for MinIO
+                                .build()
+                )
+                .build();
+    }
+}
+
+

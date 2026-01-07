dc14425e8b4e185c122c7c42c0e12368f08b9a29
Daniel Naro <dnaro@ac.upc.edu>
2026-01-07 10:40:27 +0100

    Making the RSA keys bigger

--- docker_CA/generateKeys.sh
+++ docker_CA/generateKeys.sh
@@ -64,19 +64,19 @@ touch index.txt
 echo '01' > serial.txt
 
 #Generate key for consumer (i.e. signer 1)
-openssl genpkey -algorithm RSA -out consumer_key.pem -pass pass:consumer_key -aes256
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:3072 -out consumer_key.pem -pass pass:consumer_key -aes256
 openssl req -new -key consumer_key.pem -out signing_request_consumer.csr -passin pass:consumer_key -subj "/C=ES/ST=Catalunya/L=Barcelona/O=Signer inc/OU=DMAG/CN=John Doe"
 openssl ca -batch -config openssl-ca.cnf -policy signing_policy  -out consumer_signed_cert.pem -passin pass:ca_key -in signing_request_consumer.csr -extensions final_user
 openssl pkcs12 -export -out consumer.p12 -inkey consumer_key.pem -in consumer_signed_cert.pem -certfile ca_signed_cert.pem -passin pass:consumer_key -password pass:key
 
 #Generate key for provider (i.e. signer 2)
-openssl genpkey -algorithm RSA -out provider_key.pem -pass pass:provider_key -aes256
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:3072 -out provider_key.pem -pass pass:provider_key -aes256
 openssl req -new -key provider_key.pem -out signing_request_provider.csr -passin pass:provider_key -subj "/C=ES/ST=Catalunya/L=Barcelona/O=Provider inc/OU=DMAG/CN=Jane Smith"
 openssl ca -batch -config openssl-ca.cnf -policy signing_policy  -out provider_signed_cert.pem  -passin pass:ca_key -in signing_request_provider.csr -extensions final_user
 openssl pkcs12 -export -out provider.p12 -inkey provider_key.pem -in provider_signed_cert.pem -certfile ca_signed_cert.pem -passin pass:provider_key -password pass:key
 
 #Generate certificate for OCSP responder
-openssl genpkey -algorithm RSA -out localhost_key.pem -pass pass:localhost_key -aes256
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:3072 -out localhost_key.pem -pass pass:localhost_key -aes256
 openssl req -new -key localhost_key.pem -out signing_request_localhost.csr -passin pass:localhost_key -subj "/C=ES/ST=Catalunya/L=Barcelona/O=UPC/OU=DMAG/CN=localhost"
 openssl ca -batch -config openssl-ca.cnf -policy signing_policy  -out localhost.p12  -passin pass:ca_key -in signing_request_localhost.csr -extensions ocsp_cert
 
